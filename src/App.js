b3cbb54e5a06b434aca3412346c9edf98572c74d
koydirex <80644456+koydirex@users.noreply.github.com>
2021-07-23 13:28:38 +0300

    default fail

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Content from "./UI/content"
+import Title from "./UI/title"
+function App() {
+  return (
+    <div className="App">
+      <Title></Title>
+      <Content></Content>
+    </div>
+  );
+}
+
+export default App;
