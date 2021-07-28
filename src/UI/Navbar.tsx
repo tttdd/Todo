5482f39fc137a412da4e1423ebc73084910db223
ttddd <50779378+tttdd@users.noreply.github.com>
2021-07-28 15:58:51 +0300

    отформатирован код

--- src/UI/Navbar.tsx
+++ src/UI/Navbar.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const Navbar: React.FC=()=>{
-
-
-    return(
-        <nav className="navbar navbar-dark bg-dark d-flex justify-content-center nav3">
-            <h1 className="text-light">TODO</h1>
-        </nav>
-    );
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark d-flex justify-content-center nav3">
+      <h1 className="text-light">TODO</h1>
+    </nav>
+  );
 };
 export default Navbar;
+
