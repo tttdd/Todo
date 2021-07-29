d701183aa9870e1bc411c1889ff39441a90f55ba
ttddd <50779378+tttdd@users.noreply.github.com>
2021-07-29 15:47:01 +0300

    Удалено React.FC

--- src/UI/Navbar.tsx
+++ src/UI/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Navbar: React.FC = () => {
+const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark d-flex justify-content-center nav3">
       <h1 className="text-light">TODO</h1>
@@ -8,4 +8,3 @@ const Navbar: React.FC = () => {
   );
 };
 export default Navbar;
-
