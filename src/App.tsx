ba2aa51cb4ad08c8b3e74b9d43e1e17987de1b6c
ttddd <cornilov00@gmail.com>
2021-07-29 14:06:58 +0300

    удаленн React.FC

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './UI/Navbar';
 import TodoForm from './UI/TodoForm';
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <div>
       <Navbar />
